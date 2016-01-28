20fb4b1f6bc7b589cd2e35dadb686a90ba2cbe11
Ha Junsoo <kuc2477@gmail.com>
2016-01-28 21:51:52 +0900

    -

--- manage.py
+++ manage.py
@@ -12,12 +12,5 @@ app = create_app(Dev)
 manager = Manager(app)
 
 
-@manager.option('-c', '--config', default='dev', type=str,
-                choices=['dev', 'test', 'prod'])
-def run(config):
-    create_app(config.title()).run(debug=True)
-
-
-
 if __name__ == "__main__":
     manager.run()
