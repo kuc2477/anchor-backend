164ef85624e24d8eaf03b4be89d38b8b703b2f28
Ha Junsoo <kuc2477@gmail.com>
2016-04-02 21:42:08 +0900

    apply org mode to TODO

--- components/server/utils/ma.py
+++ components/server/utils/ma.py
@@ -4,18 +4,18 @@ from sqlalchemy_utils.types import JSONType
 from ..extensions import ma
 
 
-class JSONTypeCoverter(ModelConverter):
-    SQLA_TYPE_MAPPING = dict(
-        list(ModelConverter.SQLA_TYPE_MAPPING.items()) +
-        [(JSONType, (lambda *args, **kwargs:
-                     lambda *field_args, **field_kwargs:
-                     fields.List(fields.Str)))]
-    )
+def use_json_type_as_array(model_converter):
+    converter_factory = \
+        lambda *args, **kwargs: \
+        lambda *fargs, **fkwargs: fields.List(fields.Str)
+
+    model_converter.SQLA_TYPE_MAPPING.update([(JSONType, converter_factory)])
+    return model_converter
 
 
 def get_base_schema(m):
     class Schema(ma.ModelSchema):
         class Meta:
             model = m
-            model_converter = JSONTypeCoverter
+            model_converter = use_json_type_as_array(ModelConverter)
     return Schema
