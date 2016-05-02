0a702aad4183e789d75b1166959cfd3e1e8c473b
Ha Junsoo <kuc2477@gmail.com>
2016-05-02 17:49:03 +0900

    better code layout in tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -1,4 +1,7 @@
 from flask import current_app
+from .users.models import User
+from .news.models import News
+from .classifiers.models import SVM
 from .extensions import celery, scheduler
 
 
@@ -8,6 +11,10 @@ if not current_app:
     create_app_from_env()
 
 
+# =====
+# Tasks
+# =====
+
 # run news reporting cover
 run_cover = scheduler.make_task()
 
@@ -15,15 +22,12 @@ run_cover = scheduler.make_task()
 # run learning
 @celery.task
 def fit_svm(classifier_id):
-    from .news.models import News
-    from .classifiers.models import SVM
     svm = SVM.query.get(classifier_id)
     training_set = News.query.filter(News.user_id == svm.user.id).all()
     svm.fit(training_set)
 
+
 @celery.task
 def fit_svm_for(user_id):
-    from .news.models import News
-    from .users.models import User
     user = User.query.get(user_id)
     # TODO: NOT IMPLEMENTED YET
