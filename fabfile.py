2ec88b0b8eefbdef062c153c7b17fbb2e2ad120f
Ha Junsoo <kuc2477@gmail.com>
2016-05-03 13:06:04 +0900

    add fabfile skeleton

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,44 @@
 from fabric.api import *
+
+
+VHOST = 'anchor'
+
+
+def _clone_repo():
+    # TODO: NOT IMPLEMENTED YET
+    pass
+
+
+def _update_repo():
+    # TODO: NOT IMPLEMENTED YET
+    pass
+
+
+def _reload_webserver():
+    # TODO: NOT IMPLEMENTED YET
+    pass
+
+
+def _reload_supervisor():
+    # TODO: NOT IMPLEMENTED YET
+    pass
+
+
+def _start_app():
+    # TODO: NOT IMPLEMENTED YET
+    pass
+
+
+def _reload_app():
+    # TODO: NOT IMPLEMENTED YET
+    pass
+
+
+def init_deployment():
+    # TODO: NOT IMPLEMENTED YET
+    pass
+
+
+def deploy():
+    _update_repo()
+    _reload_app()
